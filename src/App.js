351944bfa382508187664a89a67b9cfdd6700fe4
FirozSkAhmad <108452188+FirozSkAhmad@users.noreply.github.com>
2023-01-21 20:13:28 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './style.css';
+import Table from 'react-bootstrap/Table';
 
 // const names = ['divya', 'pooja', 'sonali', 'shabiya'];
 
 const details = [
-  { name: 'divya', email: 'divya@gmail.com' },
-  { name: 'divya', email: 'divya@gmail.com' },
-  { name: 'divya', email: 'divya@gmail.com' },
+  { name: 'divya', email: 'divya@gmail.com',contact:111 },
+  { name: 'divya', email: 'divya@gmail.com',contact:111 },
+  { name: 'divya', email: 'divya@gmail.com',contact:111 }
 ];
 
 export default function App() {
@@ -15,22 +16,26 @@ export default function App() {
       {/* {names.map((name, id) => (
         <h2 key={id}>My Name is:{name}</h2>
       ))} */}
-      <table border="1">
+      <Table striped bordered hover variant="dark">
         <thead >
           <tr>
+            <td>Id</td>
             <td>name</td>
             <td>email</td>
+            <td>contact</td>
           </tr>
         </thead>
         <tbody>
           {details.map((data, id) => (
             <tr key={id}>
+              <td>{id+1}</td>
               <td>{data.name}</td>
               <td>{data.email}</td>
+              <td>{data.contact}</td>
             </tr>
           ))}
         </tbody>
-      </table>
+      </Table>
     </div>
   );
 }
