83e2c60ac4eb59f09212fa527578342153078f5d
FirozSkAhmad <108452188+FirozSkAhmad@users.noreply.github.com>
2023-01-21 19:03:23 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,50 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
 export default function App() {
-  const [name, setName] = useState('');
-  const [intrest, setIntrest] = useState('');
-  const [tnc, setTnc] = useState(false);
-  const [status, setStatus] = useState(false);
-  function getData(e) {
-    console.log(name, intrest, tnc);
-    e.preventDefault();
-  }
-  function clear() {
-    setName('');
-    setIntrest('');
-    setTnc(false);
-  }
+  const [name, setName] = useState('firoz');
+  useEffect(() => {
+    console.log('useEffect');
+  });
   return (
     <div>
-      <h1>Basic Form</h1>
-      <form onSubmit={getData}>
-        <input
-          type="text"
-          placeHolder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <br />
-        <br />
-        <select onChange={(e) => setIntrest(e.target.value)} value={intrest}>
-          <option>--select option--</option>
-          <option>Marvel</option>
-          <option>DC</option>
-        </select>
-        <br />
-        <br />
-        <input
-          type="checkBox"
-          onChange={(e) => setTnc(e.target.checked)}
-          value={tnc}
-        />
-        <span>Accept all terms and conditions</span>
-        <br />
-        <br />
-        <button type="submit">Submit</button>
-        <button onClick={clear}>Clear</button>
-      </form>
+      <h1>Hello {name}</h1>
+      <button onClick={() => setName("myName")}>Update Name</button>
     </div>
   );
 }
