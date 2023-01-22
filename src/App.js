ba724233890b4880b48afc4dfedaed99b863b460
FirozSkAhmad <108452188+FirozSkAhmad@users.noreply.github.com>
2023-01-22 11:35:58 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -6,18 +6,26 @@ class App extends Component {
   constructor() {
     super();
     this.inputRef = createRef();
+    this.state = {
+      val: '',
+    };
   }
-  updateInput(){
-    console.log(this.inputRef.current.value)
-    this.inputRef.current.focus()
-    this.inputRef.current.style.color="red"
+  updateInput() {
+    console.log(this.inputRef.current.value);
+    this.inputRef.current.focus();
+    this.inputRef.current.style.color = 'red';
   }
   render() {
     return (
       <div>
         <h1>Hello !</h1>
-        <input type="text" ref={this.inputRef}/>
-        <button onClick={()=>this.updateInput()}>Click Me</button>
+        <input
+          type="text"
+          value={this.state.val}
+          ref={this.inputRef}
+          onChange={(e) => this.setState({ val: e.target.value })}
+        />
+        <button onClick={() => this.updateInput()}>Click Me</button>
         <User state="..." />
       </div>
     );
