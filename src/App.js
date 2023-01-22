29bcb54918ff9eb53537db0144205b2cf9d4c91b
FirozSkAhmad <108452188+FirozSkAhmad@users.noreply.github.com>
2023-01-22 09:48:37 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './style.css';
 
 export default function App() {
   const [name, setName] = useState('firoz');
+  const [count, setCount] = useState(0);
   useEffect(() => {
     console.log('useEffect');
   });
   return (
     <div>
-      <h1>Hello {name}</h1>
-      <button onClick={() => setName("myName")}>Update Name</button>
+      <h1>Hello {name} {count}</h1>
+      <button onClick={() => {setName('myName');setCount(count)}}>Update Name</button>
     </div>
   );
 }
