583d2d0418d646e0df6ac59be2ca48480b3c73f4
FirozSkAhmad <108452188+FirozSkAhmad@users.noreply.github.com>
2023-01-22 10:32:12 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 import './style.css';
 import User from './User.js';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      name: 'firoz',
-    };
+    this.inputRef = createRef();
+  }
+  updateInput(){
+    console.log(this.inputRef.current.value)
+    this.inputRef.current.focus()
+    this.inputRef.current.style.color="red"
   }
   render() {
     return (
       <div>
-        <h1>Hello {this.state.name}!</h1>
+        <h1>Hello !</h1>
+        <input type="text" ref={this.inputRef}/>
+        <button onClick={()=>this.updateInput()}>Click Me</button>
         <User state="..." />
       </div>
     );
