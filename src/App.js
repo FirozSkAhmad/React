97d1e2068bcd53c7444115215aeed3978c5f7b28
FirozSkAhmad <you@example.com>
2023-01-24 13:32:05 +0530

    done with get  post delete

--- src/App.js
+++ src/App.js
@@ -1,50 +1,90 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
+import { Table } from 'react-bootstrap'
 
 export default function App() {
-  const [name, setName] = useState('');
-  const [intrest, setIntrest] = useState('');
-  const [tnc, setTnc] = useState(false);
-  const [status, setStatus] = useState(false);
-  function getData(e) {
-    console.log(name, intrest, tnc);
-    e.preventDefault();
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    getUsers()
+  }, [])
+  // fetch('http://localhost:4000/todo')//Get method
+  //   .then((result) => {
+  //     result.json().then((resp) => console.log(resp))
+  //   })
+
+  function getUsers() {
+    fetch('http://localhost:4000/todo').then((result) => {
+      result.json().then((resp) => {
+        setUsers(resp)
+      })
+    })
   }
-  function clear() {
-    setName('');
-    setIntrest('');
-    setTnc(false);
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [mobile, setMobile] = useState("")
+  function getDetails(e) {
+    console.log({ name, email, mobile })
+    const data = { name, email, mobile }
+    fetch('http://localhost:4000/todo',//POST Method
+      {
+        method: "POST",
+        headers: {
+          'Accept': 'application/json',
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      }).then((result) => result.json().then((res) => {
+        console.log(res)
+        getUsers()
+      }))
+    e.preventDefault()
+  }
+
+  function remove(id) {
+    fetch(`http://localhost:4000/todo/${id}`, {
+      method: 'DELETE'
+    }).then((result) => result.json().then((res) => {
+      console.log(res)
+      getUsers()
+    }))
+
   }
   return (
-    <div>
-      <h1>Basic Form</h1>
-      <form onSubmit={getData}>
-        <input
-          type="text"
-          placeHolder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <br />
-        <br />
-        <select onChange={(e) => setIntrest(e.target.value)} value={intrest}>
-          <option>--select option--</option>
-          <option>Marvel</option>
-          <option>DC</option>
-        </select>
-        <br />
-        <br />
-        <input
-          type="checkBox"
-          onChange={(e) => setTnc(e.target.checked)}
-          value={tnc}
-        />
-        <span>Accept all terms and conditions</span>
-        <br />
-        <br />
-        <button type="submit">Submit</button>
-        <button onClick={clear}>Clear</button>
+    <>
+      <h1>Post API</h1>
+      <form onSubmit={getDetails}>
+        <input type="text" name="name" value={name} placeholder="Name" onChange={(e) => setName(e.target.value)} />
+        <input type="text" name="email" value={email} placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+        <input type="text" name="mobile" value={mobile} placeholder="Mobile" onChange={(e) => setMobile(e.target.value)} />
+        <button type='submit'>Submit</button>
       </form>
-    </div>
-  );
+
+      <h1>Data</h1>
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Mobile</th>
+            <th>Operations</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            users.map((item, id) =>
+              <tr key={id}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.mobile}</td>
+                <td><button onClick={() => remove(item.id)}>Delete</button></td>
+              </tr>
+            )
+          }
+        </tbody>
+      </Table>
+    </>
+  )
 }
